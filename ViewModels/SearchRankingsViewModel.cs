2a9aac408dd28c41bd87ca754f1560c89de600bd
Jordan Raine <raine.jordan@gmail.com>
2022-01-13 23:45:10 +1100

    UI: basic layout

---
+++ ViewModels/SearchRankingsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace ViewModels
+{
+    public sealed class SearchRankingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private string searchText;
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                OnPropertyChanged(nameof(SearchText));
+            }
+        }
+
+        private string url;
+        public string Url
+        {
+            get => url;
+            set
+            {
+                url = value;
+                OnPropertyChanged(nameof(Url));
+            }
+        }
+
+        private string rankings;
+        public string Rankings
+        {
+            get => rankings;
+            set
+            {
+                rankings = value;
+                OnPropertyChanged(nameof(Rankings));
+            }
+        }
+
+        public SearchRankingsViewModel()
+        {
+            Rankings = "TEST??";
+        }
+    }
+}
