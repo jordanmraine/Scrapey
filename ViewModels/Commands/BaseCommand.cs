a1552c93e8ffd4a18512b78e20b28b6074319db5
Jordan Raine <raine.jordan@gmail.com>
2022-01-14 00:22:04 +1100

    Views: handle searching on Search Rankings window

---
+++ ViewModels/Commands/BaseCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModels.Commands
+{
+    public class BaseCommand : ICommand
+    {
+        private readonly Action targetExecuteMethod;
+        private readonly Func<bool> targetCanExecuteMethod;
+
+        public BaseCommand(Action executeMethod)
+        {
+            targetExecuteMethod = executeMethod;
+        }
+
+        public BaseCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            targetExecuteMethod = executeMethod;
+            targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (targetCanExecuteMethod != null)
+            {
+                return targetCanExecuteMethod();
+            }
+
+            if (targetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            targetExecuteMethod?.Invoke();
+        }
+    }
+}
--- ViewModels/SearchRankingsViewModel.cs
+++ ViewModels/SearchRankingsViewModel.cs
@@ -1,4 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+
+using Models.Clients;
+using Models.Parsers;
+using Models.Services;
+using ViewModels.Commands;
 
 namespace ViewModels
 {
@@ -16,6 +24,8 @@ namespace ViewModels
             {
                 searchText = value;
                 OnPropertyChanged(nameof(SearchText));
+
+                SearchCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -27,6 +37,8 @@ namespace ViewModels
             {
                 url = value;
                 OnPropertyChanged(nameof(Url));
+
+                SearchCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -41,9 +53,73 @@ namespace ViewModels
             }
         }
 
+        private string searchButtonText;
+        public string SearchButtonText
+        {
+            get => searchButtonText;
+            set
+            {
+                searchButtonText = value;
+                OnPropertyChanged(nameof(SearchButtonText));
+            }
+        }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                isBusy = value;
+
+                if (isBusy)
+                {
+                    SearchButtonText = "Loading...";
+                }
+                else
+                {
+                    SearchButtonText = "Get Rankings";
+                }
+
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+
+        public BaseCommand SearchCommand { get; set; }
+
+        private readonly ResultService resultService;
+        private const int maxResults = 100;
+
         public SearchRankingsViewModel()
         {
-            Rankings = "TEST??";
+            // TODO: dependency injection.
+            resultService = new(new SearchClient(new HttpClient()), new RegexSearchParser());
+            SearchCommand = new BaseCommand(OnSearch, CanSearch);
+
+            IsBusy = false;
+        }
+
+        private async void OnSearch()
+        {
+            IsBusy = true;
+
+            IEnumerable<int> searchRankings = await resultService.GetSearchRankingsAsync(SearchText, Url, maxResults);
+
+            if (searchRankings == null || !searchRankings.Any())
+            {
+                Rankings = "0";
+            }
+            else
+            {
+                Rankings = string.Join(", ", searchRankings.OrderBy(sr => sr));
+            }
+
+            IsBusy = false;
+        }
+
+        private bool CanSearch()
+        {
+            return !string.IsNullOrWhiteSpace(SearchText) && !string.IsNullOrWhiteSpace(Url);
         }
     }
 }
